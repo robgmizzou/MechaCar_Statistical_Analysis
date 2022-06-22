19515a18473ec7e796e114c288c764023b8015bb
robgmizzou <100323377+robgmizzou@users.noreply.github.com>
2022-06-21 23:16:29 -0400

    Updated with Deliverable 2 script

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(tidyverse)
 
 # Deliverable 1
 # 1.4 Read in MechaCar_mpg.csv
@@ -6,3 +7,13 @@ MechaCar_mpg <- read.csv(file='C:/Users/19193/Desktop/Class Files/Resources/Mech
 # 1.5 Run linear regression across all six variables
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg))
+
+#Deliverable 2
+# 2.2 Read in suspension_coil.csv
+Suspension_Coil <- read.csv(file='C:/Users/19193/Desktop/Class Files/Resources/MechaCar_Statistical_Analysis/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+# 2.3 Write a RScript that creates total_summary dataframe with summarize function
+total_summary <- Suspension_Coil %>% summarise(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI),SD = sd(PSI))
+# 2.4 Write a lot_summary using group_by function
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI),SD = sd(PSI), .groups = 'keep')
+
+
