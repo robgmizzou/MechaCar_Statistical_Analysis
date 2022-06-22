dd3738e2d01e3164ac41a48be7160f064bf4b26a
robgmizzou <100323377+robgmizzou@users.noreply.github.com>
2022-06-21 23:31:26 -0400

    Deliverable 3 script added, final submission

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -16,4 +16,56 @@ total_summary <- Suspension_Coil %>% summarise(Mean=mean(PSI),Median=median(PSI)
 # 2.4 Write a lot_summary using group_by function
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI),SD = sd(PSI), .groups = 'keep')
 
+#Deliverable 3
+#T-test all manufacturing variance from population mean
+t.test(Suspension_Coil$PSI, mu = 1500)
+
+#One Sample t-test
+#data:  Suspension_Coil$PSI
+#t = -1.8931, df = 149, p-value = 0.06028
+#alternative hypothesis: true mean is not equal to 1500
+#95 percent confidence interval:
+#  1497.507 1500.053
+#sample estimates:
+#  mean of x 
+#1498.78
+
+#T-test for Lot1
+t.test(subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=1500)
+
+#One Sample t-test
+#data:  subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot == "Lot1")
+#t = 0, df = 49, p-value = 1
+#alternative hypothesis: true mean is not equal to 1500
+#95 percent confidence interval:
+#  1499.719 1500.281
+#sample estimates:
+#  mean of x 
+#1500
+
+#T-test for Lot2
+t.test(subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=1500)
+
+#One Sample t-test
+#data:  subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot == "Lot2")
+#t = 0.51745, df = 49, p-value = 0.6072
+#alternative hypothesis: true mean is not equal to 1500
+#95 percent confidence interval:
+#  1499.423 1500.977
+#sample estimates:
+#  mean of x 
+#1500.2
+
+#T-test for Lot3
+t.test(subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=1500)
+
+#One Sample t-test
+#data:  subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot == "Lot3")
+#t = -2.0916, df = 49, p-value = 0.04168
+#alternative hypothesis: true mean is not equal to 1500
+#95 percent confidence interval:
+#  1492.431 1499.849
+#sample estimates:
+#  mean of x 
+#1496.14
 
